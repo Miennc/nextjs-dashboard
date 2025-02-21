00e649651a429099e4ff7e753019c4befa9644ef
Nguyễn Cao Miên <2502mien@gmail.com>
2025-02-21 15:13:43 +0700

    feat: done chapter-15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,24 +1,34 @@
 'use server';
-import { z } from 'zod';
+import {z} from 'zod';
 import postgres from 'postgres';
-import { redirect } from 'next/navigation';
-import { revalidatePath } from 'next/cache';
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+import {revalidatePath} from 'next/cache';
+import {redirect} from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string({
-        invalid_type_error: 'Please select a customer.',
-    }),
-    amount: z.coerce
-        .number()
-        .gt(0, { message: 'Please enter an amount greater than $0.' }),
-    status: z.enum(['pending', 'paid'], {
+    customerId: z.string({invalid_type_error: 'Please select a customer.',}),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'],{
         invalid_type_error: 'Please select an invoice status.',
     }),
     date: z.string(),
 });
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+
+const CreateInvoice = FormSchema.omit({id: true, date: true});
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
+
 export async function createInvoice(prevState: State, formData: FormData) {
     // Validate form using Zod
     const validatedFields = CreateInvoice.safeParse({
@@ -48,6 +58,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
     } catch (error) {
         // If a database error occurs, return a more specific error.
+        console.log(" 60 error", error);
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
@@ -57,6 +68,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+
+
 export async function updateInvoice(
     id: string,
     prevState: State,
@@ -85,23 +98,37 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
     } catch (error) {
+        console.log(" 101 error", error);
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+
 export async function deleteInvoice(id: string) {
     throw new Error('Failed to Delete Invoice');
+
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 }
 
-export type State = {
-    errors?: {
-        customerId?: string[];
-        amount?: string[];
-        status?: string[];
-    };
-    message?: string | null;
-};
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
